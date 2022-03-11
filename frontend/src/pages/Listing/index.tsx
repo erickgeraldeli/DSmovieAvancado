fbbb0e926dd103559f327321c168e2f34ef59fcc
erickgeraldeli <erickvitoriogeraldeli@gmail.com>
2022-03-11 14:44:35 -0300

    Show Score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -37,18 +37,12 @@ function Listing() {
                 <div className="row">
                     {page.content.map(movie => (
                         <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard movie={movie} />
+                            <MovieCard movie={movie} />
                     </div>
                     )
                     )}
-                    
-                    
                 </div>
             </div>
-
-
-
-
         </>
     );
 }
